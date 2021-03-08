afa320940bf202ff3491f4527e2ef796231e2b6e
Vijayender Joshi <vijayender.joshi@gmail.com>
2021-03-08 12:33:14 +0900

    fix(config/fish): gtc function

--- config/fish/functions/gtc.fish
+++ config/fish/functions/gtc.fish
@@ -1,4 +1,6 @@
 # git clone into `~/dev/` with dir structure like `githosting/org/repo`
 function gtc
-    git clone $argv[1] (string join '' '~/dev/' (string replace '.git' '' $argv[1]))
+    set -l target (string join '' "$HOME/dev/" (string replace '.git' '' (string replace 'https://' '' $argv[1])))
+    mkdir -p $target
+    git clone $argv[1] $target
 end
