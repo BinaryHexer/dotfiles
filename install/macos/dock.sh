9dd670b97a307968c8cf7dc5c94fa5fcdb73001a
Vijayender Joshi <vijayender.joshi@gmail.com>
2022-08-26 16:07:25 +0900

    chore: update scripts for apple silicon

--- install/macos/dock.sh
+++ install/macos/dock.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+DLURL=$(curl --silent "https://api.github.com/repos/kcrawford/dockutil/releases/latest" | jq -r '.assets[].browser_download_url' | grep pkg)
+curl -sL ${DLURL} -o /tmp/dockutil.pkg
+sudo installer -pkg "/tmp/dockutil.pkg" -target /
+rm /tmp/dockutil.pkg
+
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Brave Browser.app"
